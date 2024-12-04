0a4a2eb11a7bb4970311b351eca21cfe22473e8c
Yash Gupta <158316939+YashGupta45@users.noreply.github.com>
2024-12-04 15:27:37 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -6,7 +6,7 @@ from decimal import Decimal
 db_connection = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="********",
+    password="*******",
     database="stock_portfolio"
 )
 
